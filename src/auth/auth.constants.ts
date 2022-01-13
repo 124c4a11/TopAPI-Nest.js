42805f1e52192155ab381ac948e2cd79612db83c
124c4a11 <124c4a11@mail.ru>
2022-01-13 18:18:37 +0300

    Auth: login user

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,1 +1,4 @@
 export const ALREADY_REGISTER_ERR_MSG = 'This user is already registered';
+export const USER_NOT_FOUND_MSG =
+  'The user with the specified email does not exist';
+export const WRONG_PASSWORD_MSG = 'Password is wrong';
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { ModelType } from '@typegoose/typegoose/lib/types';
-import { genSaltSync, hashSync } from 'bcryptjs';
+import { compare, genSalt, hash } from 'bcryptjs';
 import { InjectModel } from 'nestjs-typegoose';
+import { USER_NOT_FOUND_MSG, WRONG_PASSWORD_MSG } from './auth.constants';
 
 import { AuthDto } from './dto/auth.dto';
 import { UserModel } from './user.model';
@@ -10,13 +12,14 @@ import { UserModel } from './user.model';
 export class AuthService {
   constructor(
     @InjectModel(UserModel) private readonly userModel: ModelType<UserModel>,
+    private readonly jwtService: JwtService,
   ) {}
 
-  async createUser({ login, password}: AuthDto) {
-    const salt = genSaltSync(10);
+  async createUser({ login, password }: AuthDto) {
+    const salt = await genSalt(10);
     const newUser = new this.userModel({
       email: login,
-      passwordHash: hashSync(password, salt),
+      passwordHash: await hash(password, salt),
     });
 
     return newUser.save();
@@ -24,5 +27,32 @@ export class AuthService {
 
   async findUser(email: string) {
     return this.userModel.findOne({ email }).exec();
+  }
+
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<Pick<UserModel, 'email'>> {
+    const user = await this.findUser(email);
+
+    if (!user) {
+      throw new UnauthorizedException(USER_NOT_FOUND_MSG);
+    }
+
+    const isCorrectPassword = await compare(password, user.passwordHash);
+
+    if (!isCorrectPassword) {
+      throw new UnauthorizedException(WRONG_PASSWORD_MSG);
+    }
+
+    return { email: user.email };
+  }
+
+  async login(email: string) {
+    const payload = { email };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
