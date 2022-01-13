08ebf4727213cbc38ef231434d5e0303dfab3aeb
124c4a11 <124c4a11@mail.ru>
2022-01-13 22:34:31 +0300

    Test (e2e): auth

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,63 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { disconnect } from 'mongoose';
+
+import { AppModule } from '../src/app.module';
+import { AuthDto } from '../src/auth/dto/auth.dto';
+
+const loginDto: AuthDto = {
+  login: 'a@a.com',
+  password: '1',
+};
+
+describe('AuthController (e2e)', () => {
+  let app: INestApplication;
+  let createdId: string;
+  let token: string;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/auth/login (POST) - success', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send(loginDto)
+      .expect(200)
+      .then(({ body }: request.Response) => {
+        expect(body.access_token).toBeDefined;
+      });
+  });
+
+  it('/auth/login (POST) - fail password', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ ...loginDto, password: '2' })
+      .expect(401, {
+        statusCode: 401,
+        message: 'Password is wrong',
+        error: 'Unauthorized',
+      });
+  });
+
+  it('/auth/login (POST) - fail login', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ ...loginDto, login: 'fail@login.com' })
+      .expect(401, {
+        statusCode: 401,
+        message: 'The user with the specified email does not exist',
+        error: 'Unauthorized',
+      });
+  });
+
+  afterAll(() => {
+    disconnect();
+  });
+});
